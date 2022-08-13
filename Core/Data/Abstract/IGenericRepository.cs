abd93afcbd6e5ff7f4d3406a584111b1b77984d8
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-08-13 17:35:57 +0300

    Modified CoreLayer

---
+++ Core/Data/Abstract/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Abstract
+{
+    public interface IGenericRepository<T> where T : class,IEntity,new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties); //var kullanici = repository.GetAsyync(k=>k.Id == 15);  predicate = filtre diyebiliriz.
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ Core/Entities/Abstract/DtoGetBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public abstract class DtoGetBase //Get işlemleri için Dto'ların base sınıfı oluşturduk.Override edilebilmesi için class abstract ile işaretlendi
+    {
+        //public virtual ResultStatus ResultStatus { get; set; } //virtual olarak işaretlendiği için override edilebilir oldu. 
+    }
+}
