281c83e4e94d54823de61884818142ea9b17b31f
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-08-30 21:19:49 +0300

    LoginViewModel and PasswordResetViewModel Added

---
+++ OnlineMenuProject.UI/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineMenuProject.UI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email Adresiniz")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        [Display(Name = "Şifreniz")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage ="Şifreniz en az 4 karakter olmalıdır.")]
+        public string Password { get; set; }
+
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ OnlineMenuProject.UI/Models/PasswordResetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineMenuProject.UI.Models
+{
+    public class PasswordResetViewModel
+    {
+        [Display(Name = "Email adresiniz")]
+        [Required(ErrorMessage = "Email adresi gereklidir.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+        [Display(Name = "Yeni şifreniz")]
+        [Required(ErrorMessage = "Şifre alanı boş bırakılmamalıdır")]
+        [DataType(DataType.Password)]
+        [MinLength(4,ErrorMessage = "Şifreniz en az 4 karakter olmalıdır.")]
+        [MaxLength(20,ErrorMessage = "Şifreniz en fazla 20 karakter olmalıdır.")]
+        public string PasswordNew { get; set; }
+    }
+}
